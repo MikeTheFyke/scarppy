2d96e0c64e2a30685a78f626db361e470f6eafc1
MikeTheFyke <mikefyke@hotmail.com>
2025-02-18 16:54:28 -0500

    Updated font imports waight attrib

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,10 @@ const geistMono = Geist_Mono({
 });
 
 const inter = Inter({ subsets: ["latin"] });
-const spaceGrotesk = Space_Grotesk({ subsets: ["latin"] });
+const spaceGrotesk = Space_Grotesk({
+	subsets: ["latin"],
+	weight: ["300", "400", "500", "600", "700"],
+});
 
 export const metadata: Metadata = {
 	title: "Scappy App",
