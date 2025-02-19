3e81f09ec7778defefa7ac855534ea2464c8f597
MikeTheFyke <mikefyke@hotmail.com>
2025-02-19 12:52:23 -0500

    Starting HeroCarousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Image from "next/image";
 import SearchBar from "@/components/SearchBar";
+import HeroCarousel from "@/components/HeroCarousel";
 
 const Home = () => {
 	return (
 		<>
-			<section className="px-6 md:px-20 py-24 border-2 border-red-500">
-				<div className="flex mx-xl:flex-col gap-16">
+			<section className="px-6 md:px-20 py-24">
+				<div className="flex max-xl:flex-col gap-16">
 					<div className="flex flex-col justify-center">
 						<p className="small-text">
 							Shopping Starts Here:
@@ -26,8 +27,8 @@ const Home = () => {
 							convert, engage and retain more.
 						</p>
 						<SearchBar />
-						HeroCarousel
 					</div>
+					<HeroCarousel />
 				</div>
 			</section>
 			<section className="trending-section">
