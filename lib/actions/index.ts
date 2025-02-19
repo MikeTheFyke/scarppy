f7f38880e50394ebe2163e257fc17724f1c84739
MikeTheFyke <mikefyke@hotmail.com>
2025-02-19 15:28:23 -0500

    Building first server action scrapeAndStoreProdcut

---
+++ lib/actions/index.ts
@@ -0,0 +1,11 @@
+"use server";
+
+export async function scrapeAndStoreProduct(productURL: string) {
+	if (!productURL) return;
+
+	try {
+		const scrapedProduct = await scrapeAmazonProduct(productURL);
+	} catch (error: any) {
+		throw new Error(`Failed to create/update product: ${error.message}`);
+	}
+}
