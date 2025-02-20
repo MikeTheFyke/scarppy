3b04153077d0873a2851381c079c47f491538a16
MikeTheFyke <mikefyke@hotmail.com>
2025-02-20 17:21:57 -0500

    Added inline styles to product card

--- types/index.ts
+++ types/index.ts
@@ -14,6 +14,7 @@ export type Product = {
 	title: string;
 	currentPrice: number;
 	originalPrice: number;
+	concatPrice: number;
 	priceHistory: PriceHistoryItem[] | [];
 	highestPrice: number;
 	lowestPrice: number;
