4fdca2876297be29c3bbfd1453fd833e1880f30e
MikeTheFyke <mikefyke@hotmail.com>
2025-02-21 17:52:09 -0500

    Product Details page adding heart icon

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { getProductById } from "@/lib/actions";
 import { redirect } from "next/navigation";
 import Image from "next/image";
+import Link from "next/link";
 
 type Props = {
 	params: { id: string };
@@ -23,7 +24,32 @@ const ProductDetails = async ({ params: { id } }: Props) => {
 						className="mx-auto"
 					/>
 				</div>
-				<div></div>
+				<div className="flex flex-1 flex-col">
+					<div className="flex justify-between items-start gap-5 flex-wrap pb-6">
+						<div className="flex flex-col gap-3">
+							<p className="text-[28px] text-secondary font-semibold">
+								{product.title}
+							</p>
+							<Link
+								href={product.url}
+								target="_blank"
+								className="text-base text-black opacity-50"
+							>
+								Visit Product
+							</Link>
+						</div>
+						<div className="flex items-center gap-3">
+							<div className="product-hearts">
+								<Image
+									src={"/assets/icons/red-heart-svg"}
+									alt="heart"
+									width={20}
+									height={20}
+								/>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
