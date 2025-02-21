29061419c3b08330e3ae2f18c0712183f5587504
MikeTheFyke <mikefyke@hotmail.com>
2025-02-21 17:45:20 -0500

    Product details fetched product image based off of id params

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { getProductById } from "@/lib/actions";
 import { redirect } from "next/navigation";
+import Image from "next/image";
 
 type Props = {
 	params: { id: string };
@@ -10,7 +11,22 @@ const ProductDetails = async ({ params: { id } }: Props) => {
 
 	if (!product) redirect("/");
 
-	return <div>{id}</div>;
+	return (
+		<div className="product-container">
+			<div className="flex gap-28 xl:flex-row flex-col">
+				<div className="product-image">
+					<Image
+						src={product.image}
+						alt={product.title}
+						width={580}
+						height={480}
+						className="mx-auto"
+					/>
+				</div>
+				<div></div>
+			</div>
+		</div>
+	);
 };
 
 export default ProductDetails;
